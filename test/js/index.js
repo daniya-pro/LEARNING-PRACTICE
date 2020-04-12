570541fac9e41e4bcd7d1ac1f2932a8bb445255d
Daniya <syedadaniyahaider@gmail.com>
2020-04-13 01:33:14 +0500

    new folder test

---
+++ test/js/index.js
@@ -0,0 +1,27 @@
+var space = name.indexOf(' ')
+var spacei = ' '
+var name = ["Adam pagal", "Alex lol", "Aaron mad", "Ben mental", "Carl gadha", "Daniya haider", "David worship", "Edward falsegod", ]
+
+
+var firstchars = []
+
+var /*new*/ firstandlast = []
+
+var lastchars = []
+
+var otherchar = []
+
+var first = []
+
+var last = []
+
+for (var i = 0; i < array.length; i++) {
+
+    lastchars[i] = name.slice(space, space + 1)
+
+
+    firstchars[i] = name.slice(0, 1)
+
+    first[i] = name.splice
+
+}
