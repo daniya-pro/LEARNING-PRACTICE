da8da242dc8bb98a6ffb4e7d3a89cd68a7985c86
Daniya <syedadaniyahaider@gmail.com>
2020-04-12 22:59:15 +0500

    star generator aded

--- from othr laptp/5-4-20/js/loop.js
+++ from othr laptp/5-4-20/js/loop.js
@@ -5,5 +5,21 @@ for (var i = 10; i > 0; i--) {
     consol.log(i)
 }
 
+/*stars
+ */
 
-var N = []
+var number = parseInt(prompt("HOW MANY STARS DO YOU WANT TO GENERATE????? "))
+
+document.write("<h3>Here are you stars:- </h3><br/>")
+var stars = ""
+for (0; number > 0; number--) {
+    console.log(number)
+
+    for (var i = 0; i < number; i++) {
+        stars += "*"
+    }
+    stars += "<br>"
+
+}
+
+document.write(stars)
