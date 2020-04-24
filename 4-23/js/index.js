eb118d6be7aa4b425396d1da565997b61ff54a5d
Daniya <daniya@git.com>
2020-04-24 21:37:19 +0500

    madded a website of animation

---
+++ 4-23/js/index.js
@@ -0,0 +1,64 @@
+function colorchange(this1) {
+  if (this1.innerText == "MOUSE IS ON") {
+    this1.innerText = "MOUSE IS OFF";
+  } else {
+    this1.innerText = "MOUSE IS ON";
+  }
+}
+
+function colorchange2(this2) {
+  //this2.setAttribute("style", "background-color: yellow;");3
+  this2.style.backgroundColor = "yellow";
+}
+function colorchange23(this23) {
+  this23.style.backgroundColor = "white";
+}
+function colorchange3(this3) {
+  //  this3.setAttribute("src", "../images/04cb5a80834ece33456879660d8fc00b.png");
+
+  //alert(this3.src)
+  if (
+    b.src ==
+    "file:///C:/Users/DANIYA/Desktop/work/LEARNING-PRACTICE/images/04cb5a80834ece33456879660d8fc00b8.png"
+  ) {
+    b.setAttribute("src", "../images/04cb5a80834ece33456879660d8fc00b.png");
+  } else {
+    b.setAttribute("src", "../images/04cb5a80834ece33456879660d8fc00b8.png");
+  
+  
+  }
+var omg = document.getElementById('o')
+  if (
+    omg.src ==
+    "file:///C:/Users/DANIYA/Desktop/work/LEARNING-PRACTICE/images/off.png"
+  ) {
+    omg.setAttribute("src", "../images/on.png");
+  } else {
+    omg.setAttribute("src", "../images/off.png");
+  }
+}
+var color;
+alert("box pr click kro ");
+function colorchange52() {
+  color = prompt(
+    "color batao box ka ya phr apna hi koi favourite color batao agr color galt hhua to tumhe koi respnd nhi milega ok?"
+  );
+  alert("pr hover karo");
+}
+function colorchange5(this5) {
+  this5.style.backgroundColor = color;
+}
+
+function colorchange4(this4) {
+  if (this4.style.marginLeft === "0px") {
+    this4.style.marginLeft = "1199px";
+  } else {
+    this4.style.marginLeft = "0";
+  }
+}
+
+function checkAddress() {
+  if (document.getElementById("email").value === "") {
+    alert("Email address required.");
+  }
+}
---
+++ test 2/js/index.js
@@ -0,0 +1,30 @@
+function whilefor(i, I) {
+  alert("for vs while diff betweeen for loop and while do you wanna see diffs");
+  alert("let see diffrence of while loop");
+
+  while (i < 5) {
+    alert("this is our number " + i + " loop oops i mean while loop 😊😁");
+    i++;
+  }
+  alert("let see diffrence of for loop");
+  for (I; I < 5; I++) {
+    alert("this is our number" + I + " loop oops i mean for loop 🙄😒");
+  }
+  alert(
+    "did you find any diff between while loop and for loop oh come on🙄 its not a diff😒 ok i'll tell you the diffrence there is no differnce in alert the diffrence is that we just code them in diff ways for example : (just press okay to see the example alert)"
+  );
+  alert(
+    "for(var i = 0; i < 5; i++) { \n alert('this is our for loop 🙄😒) \n \n now see while loop \n var i = 0 while (i < 4) { \n alert(this is our while loop 😊😁 );i++;"
+  );
+  alert(
+    "do you understand the dif is between codding oh what u didnt see coddings oh ok now i am gonna show you my codding again this time pls focus"
+  );
+  alert(
+    "for(var i = 0; i < 5; i++) { \n alert('this is our for loop 🙄😒) \n \n now see while loop \n var i = 0 while (i < 4) { \n alert(this is our while loop 😊😁 );i++;"
+  );
+  alert(
+    "now you understand if no u must be a very week clumsy and dirty student and  and if yes oh you must be a very clever clean smart intelligent student "
+  );
+}
+whilefor(1, 1);
+alert("Error:this website has an Error the code is wrong thats why you will get so many alerts (that doesn't mean it will stop afer a day it will never stop). Error Name : alerts attack. (if you want to (resque)save your computer from ths Error so just leave now ) Error Code : 56y75." )
