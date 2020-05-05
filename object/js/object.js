49170c8e671c10aa7f3103fd59d84f5391dbcc15
Daniya <syedadaniyahaider@gmail.com>
2020-05-06 03:30:12 +0500

    ''

--- object/js/object.js
+++ object/js/object.js
@@ -4,19 +4,19 @@ var plans = [
     
     {
         name: "Basic",
-        price: 2.99,
+        price: 3.99,
         space: 100,
         transfer: 1000,
         pages: 10,
-        discountMonths: [2,6, 7]
+        discountMonths: [4,6, 7]
     },
     {
-        name: "Developes",
-        price: 4.99,
-        space: 200,
-        transfer: 2000,
+        name: "Professional",
+        price: 5.99,
+        space: 500,
+        transfer: 5000,
         pages: 50,
-        discountMonths: [6, 7,9]
+        discountMonths: [6, 7,11]
     },
      {
         name: "Premium",
@@ -24,13 +24,14 @@ var plans = [
         space: 300,
         transfer: 3000,
         pages: 500,
-        discountMonths: [6, 8]
+        discountMonths: [0, 1,2]
     }
 ]
 
 
 
  function getMonthsNames(monthsArray){
+   
   var MonthsNames = ''
     for(var I =0;I<monthsArray.length;I++){
         var MonthNum = monthsArray[I]
@@ -38,6 +39,7 @@ var plans = [
     }
    return MonthsNames
  }
+
 for (var i = 0; i < plans.length; i++) {
     console.log(plans[i].name)
     console.log(plans[i].price)
@@ -64,19 +66,62 @@ var htmlCard =`
       <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Free Subdomain</li>
       <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Monthly Status Reports</li>
     </ul>
-    <a href="#" class="btn btn-block btn-primary text-uppercase">Button</a>
-  </div>
-</div>
-</div>
+    <a class="btn btn-block btn-primary" onclick="onclicks(${i})" data-toggle="modal" data-target="#myModal">Buy Now!</a>
 `
 document.getElementById("rw").innerHTML += htmlCard
    
 }
+function onclicks(num){
+
+  var plan = plans[num]
+  console.log("specific obj=>>> ",plan)
+ var now = new Date().getMonth();
+for (let i = 0; i < months.length; i++) {
+  if(now === plan.discountMonths[i]){
+
+
+var totlee=plan.price*.2
+var totle =plan.price-totlee 
+var discount="20%"
+break;
+  }
+  else{var totle =plan.price;var discount="0%";
+  break
+}
+}
+console.log(discount)
+var s$ = "$"
+var h=`<table style="font-size: 19px;font-family: a;">
+<tbody>
+<tr>
+<td><strong>Sub-Total : </strong>${s$+plan.price}</td>
+</tr>
+<tr>
+<td><strong>Tax : </strong> $0</td>
+</tr>
+<tr>
+<td>
+<strong>Discount : </strong>${discount}
+</td>
+</tr>
+<tr>
+<td><strong>Total : </strong>${s$+totle}</td>
+
+</tr>
+</tbody>
+</table> `
+var p =plan.name
+document.getElementById("h").innerHTML=h
+document.getElementById("exampleModalLongTitle").innerHTML=p
+
+}
+
 
 function removeLoading(){ 
     document.getElementById("s").remove();
     document.getElementById("rw").style.display=""
+    document.getElementById("str").style.display=""
 
 }
 
-setTimeout(removeLoading, 1000);
+setTimeout(removeLoading, 1000)
